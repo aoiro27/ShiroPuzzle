d6525589f2e41f04845a2f1d54a68bc2adf8aa1a
yuki.u <aoiro.savant@gmail.com>
2026-02-21 08:52:14 +0900

    Initial Commit

---
+++ ShiroPuzzle/ShiroPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiroPuzzleApp.swift
+//  ShiroPuzzle
+//
+//  Created by Yuki Usui on 2026/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ShiroPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
